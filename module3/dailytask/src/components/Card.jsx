6af8f1ab36cfd38befbec2abc4b5357513a38733
Srushti mokashi <srushtimokashi5@gmail.com>
2025-09-14 23:22:24 +0530

    Added project

---
+++ module3/dailytask/src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Card() {
+    return <h1>Hello Card Component</h1>;
+}
+
+export default Card;
+
+
---
+++ module3/dailytask/src/components/CreateTodo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateTodo = () => {
+  return (
+    <div>
+      <h1>Hello CreateTodo</h1>
+    </div>
+  )
+}
+
+export default CreateTodo
