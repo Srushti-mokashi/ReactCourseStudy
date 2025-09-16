639b0fb67a3503aaaee1a1ef1d187033f64b0a5c
Srushti mokashi <srushtimokashi5@gmail.com>
2025-09-16 22:02:02 +0530

    Updated dailytask

--- module3/dailytask/src/components/CreateTodo.jsx
+++ module3/dailytask/src/components/CreateTodo.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
 
 const CreateTodo = () => {
-  return (
-    <div>
-      <h1>Hello CreateTodo</h1>
-    </div>
-  )
-}
+  let task = 5;
+  const isDisabled = true;
+  // const styles = {
+  //   color: "red"
+  // };
+  const countTask = () => {
+    // if (task === 0){
+    //   return "No task available";
+    // }else{
+    //    return `Total Task ${task}`;
+    // }
+    
+    return task === 0 ? "No Task Available .!!" : `Total Task ${task}`;
+};
 
-export default CreateTodo
+    const handleClick = (e) =>{
+      task++;
+      console.log("Button Clicked.!!", task);
+    
+  };
+  return (
+    <>
+      <h1 style={{ color: "red"}} className="btn">{task} </h1>
+      <button disabled={isDisabled} value="Add Task button"  onClick={handleClick}>Add Task</button>
+    </>
+  );
+};
+
+export default CreateTodo;
