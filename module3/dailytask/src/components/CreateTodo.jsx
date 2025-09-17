9fe4e62f6d4603bbe73bb220f51eb784a79ffc7e
Srushti mokashi <srushtimokashi5@gmail.com>
2025-09-17 22:22:24 +0530

    Updated User Input in forms

--- module3/dailytask/src/components/CreateTodo.jsx
+++ module3/dailytask/src/components/CreateTodo.jsx
@@ -3,13 +3,19 @@ import { useState } from "react";
 const CreateTodo = () => {
   
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
   const isDisabled = false;
   const handleClick = () =>{
       setCount(count + 1);
     };
+
+    const handleChange = (event) => {
+     setInput(event.target.value);
+    };
     return (
     <>
       <h1 style={{ color: "black"}}>{count}</h1>
+      <input type="text" onChange="handleChange" value={input}/>
       <button 
        disabled={isDisabled} 
        value="Add Task button"  
@@ -17,6 +23,7 @@ const CreateTodo = () => {
      >
        
          Add Task</button>
+         <h2>{input}</h2>
     </>
   );
 };
