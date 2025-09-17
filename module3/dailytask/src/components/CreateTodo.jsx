549cae6999feaf00fc54a9f3169211fe3c6b4584
Srushti mokashi <srushtimokashi5@gmail.com>
2025-09-17 21:46:43 +0530

    Updated useState in dailytask

--- module3/dailytask/src/components/CreateTodo.jsx
+++ module3/dailytask/src/components/CreateTodo.jsx
@@ -1,29 +1,22 @@
+import { useState } from "react";
 
 const CreateTodo = () => {
-  let task = 5;
-  const isDisabled = true;
-  // const styles = {
-  //   color: "red"
-  // };
-  const countTask = () => {
-    // if (task === 0){
-    //   return "No task available";
-    // }else{
-    //    return `Total Task ${task}`;
-    // }
-    
-    return task === 0 ? "No Task Available .!!" : `Total Task ${task}`;
-};
-
-    const handleClick = (e) =>{
-      task++;
-      console.log("Button Clicked.!!", task);
-    
-  };
-  return (
+  
+  const [count, setCount] = useState(0);
+  const isDisabled = false;
+  const handleClick = () =>{
+      setCount(count + 1);
+    };
+    return (
     <>
-      <h1 style={{ color: "red"}} className="btn">{task} </h1>
-      <button disabled={isDisabled} value="Add Task button"  onClick={handleClick}>Add Task</button>
+      <h1 style={{ color: "black"}}>{count}</h1>
+      <button 
+       disabled={isDisabled} 
+       value="Add Task button"  
+       onClick={handleClick}
+     >
+       
+         Add Task</button>
     </>
   );
 };
