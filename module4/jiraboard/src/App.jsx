92876eb7d957a11552059c982ac996c65cfa499d
Srushti mokashi <srushtimokashi5@gmail.com>
2025-09-18 19:21:36 +0530

    Added components in jiraboard

--- module4/jiraboard/src/App.jsx
+++ module4/jiraboard/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './app.css';
+import TaskForm from './components/TaskForm/task-form';
 const App = () => {
   return (
     <div className="app">
-     <header className="app_header">Jira Board</header>
+    <h1 style={{ textAlign: "center", margin: "50px"}}>Jira Board</h1>
+     <TaskForm/>
      <main className="app_main">
        <section className="task_column">Section 1</section>
        <section className="task_column">Section 2</section>
---
+++ module4/jiraboard/src/components/TaskForm/task-form.jsx
@@ -0,0 +1,31 @@
+import './task-form.css'; 
+
+const TaskForm = () => {
+  return (
+    
+       <header className="app_header">
+          <form>
+            <input
+            type="text" 
+            className="task_input" 
+            placeholder="Enter task details"
+         />
+         <div className="">
+            <button className="tag">DEV</button>
+            <button className="tag">QA</button>
+            <button className="tag">Product Owner</button>
+         </div>
+         <select className="task_status">
+            <option value="Ready for Development">Ready for Development</option>
+            <option value="In Progress">In Progress</option>
+            <option value="Ready for tes">Ready for test</option>
+            <option value="Closed">Closed</option>
+         </select>
+         <button type="submit" className="task_submit">+ Add</button>
+        </form>
+       </header>
+    
+  );
+};
+
+export default TaskForm;
