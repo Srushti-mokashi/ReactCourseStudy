f9205aacea5bd018085546dca16013723d2ce55e
Srushti mokashi <srushtimokashi5@gmail.com>
2025-09-18 12:29:35 +0530

    Props- Passing data between components

--- module3/dailytask/src/components/Card.jsx
+++ module3/dailytask/src/components/Card.jsx
@@ -1,9 +1,16 @@
-import React from "react";
 
-function Card() {
-    return <h1>Hello Card Component</h1>;
+import CreateTodo from './CreateTodo';
+import UserCard from './UserCard';
+
+const Card = () => {
+  return (
+    <div>
+      <h1>Hello Card Components</h1>
+      <CreateTodo />
+      <UserCard name="Neha" profession="Sofware engineer"/>
+     <UserCard name="Sneha" profession="DevOps engineer"/>
+    </div>
+  )
 }
 
-export default Card;
-
-
+export default Card
