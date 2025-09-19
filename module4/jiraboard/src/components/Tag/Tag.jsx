96939bc03bff41af1375ab1843d619247ee3ef42
Srushti mokashi <srushtimokashi5@gmail.com>
2025-09-19 15:32:35 +0530

    Creating a Reusable Tag Component

---
+++ module4/jiraboard/src/components/Tag/Tag.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./tag.css";
+const Tag = () => {
+  return (
+    <>
+        <button className='tag'>DEV</button>
+    </>
+  );
+};
+
+export default Tag
--- module4/jiraboard/src/components/TaskForm/task-form.jsx
+++ module4/jiraboard/src/components/TaskForm/task-form.jsx
@@ -1,5 +1,5 @@
 import './task-form.css';
-
+import Tag from "../Tag/Tag";
 const TaskForm = () => {
   return (
     
@@ -12,9 +12,9 @@ const TaskForm = () => {
          />
          <div className="task_form_bottom">
          <div>
-            <button className="tag">DEV</button>
-            <button className="tag">QA</button>
-            <button className="tag">Product Owner</button>
+           <Tag/>
+            <Tag/>
+            <Tag/>
             </div>
         
          <div>
