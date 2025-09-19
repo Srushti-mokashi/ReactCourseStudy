1b3e0d3375fff983db3f4107c400b0c59acba7f0
Srushti mokashi <srushtimokashi5@gmail.com>
2025-09-19 17:33:54 +0530

    Creating a column component with styling

--- module4/jiraboard/src/App.jsx
+++ module4/jiraboard/src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './app.css';
 import TaskForm from './components/TaskForm/task-form';
+import TaskColumn from './components/TaskColumn/task-column';
+import closedIcon from './assets/checked.png';
 const App = () => {
   return (
     <div className="app">
     <h1 style={{ textAlign: "center", margin: "50px"}}>Jira Board</h1>
      <TaskForm/>
      <main className="app_main">
-       <section className="task_column">Section 1</section>
-       <section className="task_column">Section 2</section>
-       <section className="task_column">Section 3</section>
-       <section className="task_column">Section 4</section>
+      <TaskColumn title="Ready for Development"/>
+      <TaskColumn  title="In Progress"/>
+      <TaskColumn  title="Ready for test" />
+      <TaskColumn  title="closed" icon={closedIcon}/>
      </main>
     </div>
   )
---
+++ module4/jiraboard/src/components/TaskColumn/task-column.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./task-column.css";
+const TaskColumn = (props) => {
+  return (
+    <div>
+      <section className="task_column">
+        <h2 className="task_column_heading">
+         <img src={props.icon} alt="" className='task_column_icon'></img>
+         {props.title}
+       
+        </h2>
+      </section>
+    </div>
+  );
+};
+
+export default TaskColumn;
