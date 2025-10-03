eedb9499c04d5362d5597fdd13145ed547b7d2b2
Srushti mokashi <srushtimokashi5@gmail.com>
2025-10-03 18:31:26 +0530

    Implementing route parameters: load data dynamically

--- module6/router/src/App.jsx
+++ module6/router/src/App.jsx
@@ -7,7 +7,7 @@ import Admin from "./components/Admin/Admin";
 import Contact from './components/Contact/contact';
 import NotFound from './components/NotFound/NotFound';
 import Navbar from './components/Navbar/Navbar';
-
+import SingleProduct from './components/Products/SingleProduct';
 const App = () => {
   return (
     <div className="app">
@@ -17,6 +17,7 @@ const App = () => {
       <Routes>
        <Route path="/" element={<Home />} />
        <Route path="/products" element={<Products />} />
+       <Route path="/products/:id" element={<SingleProduct />} />
        <Route path="/articles" element={<Articles />} />
        <Route path="/admin" element={<Admin />} />
        <Route path="/contact" element={<Contact />} />
--- module6/router/src/components/Products/SingleProduct.jsx
+++ module6/router/src/components/Products/SingleProduct.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const SingleProduct = () => {
+  const { id } = useParams();
+  
   return (
     <div>
-      
+      <h2>SingleProduct - { id }</h2>
     </div>
   )
 }
