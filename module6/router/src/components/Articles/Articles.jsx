0bd2da9f6975ab99f805f37f2190a6f4daf67b13
Srushti mokashi <srushtimokashi5@gmail.com>
2025-10-03 22:59:27 +0530

    Implementing query strings using useSearchParams hook

--- module6/router/src/components/Articles/Articles.jsx
+++ module6/router/src/components/Articles/Articles.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 const Articles = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sortBy = searchParams.get("sortBy");
+  const category = searchParams.get("category");
+  const sortByViews = searchParams.get("sortByViews");
+
+  const handleSortBy = () => {
+    setSearchParams({
+      sortByViews: "Views",
+      category: "electronics",
+    });
+  };
   return (
     <div>
       <h2>Articles
-         <p>SortBy: Category:</p></h2>
+         <p>SortBy:{sortBy ?? sortByViews} Category:{category}
+         </p>
+         </h2>
+         <button onClick={handleSortBy}>sort By Views</button>
     </div>
   )
 }
