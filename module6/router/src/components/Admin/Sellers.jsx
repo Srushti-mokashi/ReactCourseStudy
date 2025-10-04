38ea10cdce17d6a05c58ffc4d6d0733a6fde1884
Srushti mokashi <srushtimokashi5@gmail.com>
2025-10-04 18:44:08 +0530

    Implementing useEffects and its dependency array: Mounting and Re-rendering

--- module6/router/src/components/Admin/Sellers.jsx
+++ module6/router/src/components/Admin/Sellers.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Sellers = () => {
+  const  [name, setName] = useState("");
+  useEffect(()=>{
+    document.title = `Name is ${name}`;
+    console.log("Component Mount.!!");
+  }, [name]);
+  
   return (
-    <div>
-      <h3>Admin Sellers Page</h3>
-    </div>
-  )
-}
+    <>
+       <h3>Admin Sellers Page</h3>
+       <input type="text"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}/>
+    </>
+  
 
-export default Sellers
+  );
+};
+
+export default Sellers;
