814d0325d830b81eee7b66aee2d0d836472b7247
Srushti mokashi <srushtimokashi5@gmail.com>
2025-10-05 22:22:46 +0530

    Fetched data from a server : Using fetch and axios

--- module6/router/src/components/Admin/Sellers.jsx
+++ module6/router/src/components/Admin/Sellers.jsx
@@ -1,22 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
 
 const Sellers = () => {
-  const  [name, setName] = useState("");
-  useEffect(()=>{
-    // document.title = `Name is ${name}`;
-    console.log("Component Mount.!!");
-    return () =>{
-      console.log("Component Unmount.!!");
-    }
-  }, [name]);
+  useState [users, setUsers] = useState([]);
+  // const  [name, setName] = useState("");
+  // useEffect(()=>{
+  //   // document.title = `Name is ${name}`;
+  //   console.log("Component Mount.!!");
+  //   return () =>{
+  //     console.log("Component Unmount.!!");
+  //   }
+  // }, [name]);
   
+  useEffect(() =>{
+    axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      setUsers(res.data);
+    });
+    //  fetch("https://jsonplaceholder.typicode.com/users")
+    //  .then((res) => res.json())
+    //  .then((data) => console.log(data)).catch((error)=>{})
+  },[])
   return (
     <>
        <h3>Admin Sellers Page</h3>
        <input type="text"
               onChange={(e) => {
                 setName(e.target.value);
-              }}/>
+              }}
+
+              />
+              {users.map((user) => (
+                <p key={user.id}>{user.name}</p>
+              ))}
     </>
   
 
