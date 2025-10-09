3603b19225f40019941dac771b64f88be7963f08
Srushti mokashi <srushtimokashi5@gmail.com>
2025-10-09 23:19:08 +0530

    Initialized routing setup before integrating Higher Order Components

--- module8/react-design-patterns/src/App.jsx
+++ module8/react-design-patterns/src/App.jsx
@@ -1,5 +1,6 @@
 import UserContainer from "./containers/UserContainer";
 import useWindowSize from "./Hooks/UseWindowSize";
+import { Routes , Route } from "react-router-dom";
 
 function App(){
   const { width, height } = useWindowSize();
@@ -10,6 +11,13 @@ function App(){
       <div>
            Window Size : {width} x {height}
       </div>
+      <hr></hr>
+
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/userprofile" element={<UserProfile/>} />
+        <Route path="/" element={<div>HOC Component and Welcome to login Page</div>} />
+      </Routes>
     </>
   );
 }
