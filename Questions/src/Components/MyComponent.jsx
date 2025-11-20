05820e8c142e271fe8ab7ebb47be14de3ffc978d
Srushti mokashi <srushtimokashi5@gmail.com>
2025-11-20 18:20:41 +0530

    Added html, css card

---
+++ Questions/src/Components/MyComponent.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from "raect";
+
+const MyComponent = () => {
+    const [count, setCount] = useState(0);
+  return (
+    <div>
+       <Display/>
+       <Controls/>
+    </div>
+  )
+};
+
+const Display(){
+
+}
+
+const Controls(){
+
+}
+
+export default MyComponent
